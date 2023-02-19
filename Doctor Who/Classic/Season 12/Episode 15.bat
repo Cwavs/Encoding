b7c1b6783d3e7607070189bbad0df4ce88b96f8d
Cwavs <16854045+Cwavs@users.noreply.github.com>
2023-02-19 10:47:47 +0000

    Add more classic Who scripts
    Finished Season 12 and started Season 19.

---
+++ Doctor Who/Classic/Season 12/Episode 15.bat
@@ -0,0 +1,4 @@
+vspipe "Episode 15.vpy" -o 3 -c y4m - | x264 --demuxer y4m --preset veryslow --level 41 --me umh --rc-lookahead 250 --profile high --crf 16 --deblock -2:-2 --no-mbtree --qcomp 0.65 --aq-mode 2 --aq-strength 0.80 --merange 32 --ipratio 1.30 --pbratio 1.20 --psy-rd 0.95 --no-dct-decimate --ref 16 -o out.264 -
+ffmpeg -i "D:\DOCTOR_WHO_GENESIS_OF_THE_DALEKS\BDMV\STREAM\00122 PID 1100 L+R 48 1536 DELAY 0ms.dts" -b:a 128k -c:a libopus "English Stereo.opus"
+ffmpeg -i "D:\DOCTOR_WHO_GENESIS_OF_THE_DALEKS\BDMV\STREAM\00122 PID 1101 C+L+R+SL+SR 48 1536 DELAY 0ms.dts" -ac 6 -b:a 256k -c:a libopus "English Surround.opus"
+ffmpeg -i "D:\DOCTOR_WHO_GENESIS_OF_THE_DALEKS\BDMV\STREAM\00122 PID 1102 L+R 48 1536 DELAY 0ms.dts" -b:a 128k -c:a libopus "English Commentary.opus"
